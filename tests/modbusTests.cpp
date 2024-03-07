bbb89c2ef4d80fb463a571c874d3bfcf76f2bbe8
Agnieszka Klempis <a.klempis@student.uw.edu.pl>
2024-03-07 18:52:37 +0100

    Add more unit tests with corrects requests.

--- tests/modbusTests.cpp
+++ tests/modbusTests.cpp
@@ -22,31 +22,27 @@ const std::string correctWriteCurrentResponse = slaveAddr
                                                 + "0012" // Quantity of coils.
                                                 + "D5C6"; // CRC
 
-const std::string correctSwitchOnOffResponse = slaveAddr
+const std::string correctWriteOneBitValueResponse = slaveAddr
                                                + modbusWriteCoils
                                                + "0000" // Address of first coil to be written.
                                                + "0003" // Quantity of coils.
                                                + "15CA";
 
-TEST(ModbusServerTest, RespondsToCorrectReadRequest) {
-    uint16_t current = std::stoi("FFF", nullptr, 16);
-    uint16_t voltage = std::stoi("FFF", nullptr, 16);
-    bool isOn = 1;
-    bool polarity = 1;
-    bool reset = 1;
-    bool remote = 1;
-    uint8_t errors = std::stoi("F", nullptr, 16);
+class ModbusServerReadTest
+: public testing::TestWithParam<std::tuple<std::string, std::string, bool, bool, bool, bool, std::string, std::string>> {
+};
+
+TEST_P(ModbusServerReadTest, RespondsToCorrectReadRequest) {
+
+    auto [currentHex, voltageHex, isOn, polarity, reset, remote, errorsHex, response] = ModbusServerReadTest::GetParam();
+
+    uint16_t current = std::stoi(currentHex, nullptr, 16);
+    uint16_t voltage = std::stoi(voltageHex, nullptr, 16);
+    uint8_t errors = std::stoi(errorsHex, nullptr, 16);
 
     std::unique_ptr<MockPowerSupply> mockPowerSupply = std::make_unique<MockPowerSupply>(current, voltage, isOn,
                                                                                          polarity, reset, remote,
                                                                                          errors);
-    std::string response = slaveAddr
-                           + modbusReadRegisters
-                           + "04" // Byte count
-                           + "FFFF" // Register 0
-                           + "FFFF" // Register 1
-                           + "FA10"; // CRC
-
     std::string request = correctReadRequest;
 
     auto mockPicoController = std::make_shared<MockPicoController>(request, response);
@@ -55,6 +51,43 @@ TEST(ModbusServerTest, RespondsToCorrectReadRequest) {
 
     modbusServer->waitAndHandleRequest();
 }
+
+INSTANTIATE_TEST_SUITE_P(
+        ModbusServerTest, ModbusServerReadTest,
+        testing::Values(
+                std::make_tuple("FFF", "FFF", 1, 1, 1, 1, "F",
+                                slaveAddr
+                                + modbusReadRegisters
+                                + "04" // Byte count
+                                + "FFFF" // Register 0
+                                + "FFFF" // Register 1
+                                + "FA10" // CRC
+                                ),
+                std::make_tuple("0", "0", 0, 0, 0, 0, "0",
+                                slaveAddr
+                                + modbusReadRegisters
+                                + "04" // Byte count
+                                + "0000" // Register 0
+                                + "0000" // Register 1
+                                + "FB84" // CRC
+                ),
+                std::make_tuple("123", "456", 0, 1, 0, 1, "1",
+                                slaveAddr
+                                + modbusReadRegisters
+                                + "04" // Byte count
+                                + "A123" // Register 0
+                                + "1456" // Register 1
+                                + "A68C" // CRC
+                ),
+                std::make_tuple("ABC", "DEF", 1, 0, 1, 0, "A",
+                                slaveAddr
+                                + modbusReadRegisters
+                                + "04" // Byte count
+                                + "5ABC" // Register 0
+                                + "ADEF" // Register 1
+                                + "15A4" // CRC
+                )
+        ));
 
 class ModbusServerWriteTest
         : public testing::TestWithParam<std::tuple<PowerSupplyParameter, int, std::string, std::string>> {
@@ -85,6 +118,24 @@ INSTANTIATE_TEST_SUITE_P(
                                 + "FFFF03" // Coils' values.
                                 + "E9B4" // CRC
                         , correctWriteCurrentResponse),
+                std::make_tuple(PowerSupplyParameter::CURRENT, std::stoi("0000", nullptr, 16),
+                                slaveAddr
+                                + modbusWriteCoils
+                                + "0000" // Address of first coil to be written.
+                                + "0012" // Quantity of coils.
+                                + "03" // Byte count.
+                                + "030000" // Coils' values.
+                                + "2875" // CRC
+                        , correctWriteCurrentResponse),
+                std::make_tuple(PowerSupplyParameter::CURRENT, 36101,
+                                slaveAddr
+                                + modbusWriteCoils
+                                + "0000" // Address of first coil to be written.
+                                + "0012" // Quantity of coils.
+                                + "03" // Byte count.
+                                + "173402" // Coils' values.
+                                + "FF70" // CRC
+                        , correctWriteCurrentResponse),
                 std::make_tuple(PowerSupplyParameter::IS_ON, 1,
                                 slaveAddr
                                 + modbusWriteCoils
@@ -93,7 +144,16 @@ INSTANTIATE_TEST_SUITE_P(
                                 + "01" // Byte count.
                                 + "04" //Coils' values.
                                 + "8E94" // CRC
-                        , correctSwitchOnOffResponse),
+                        , correctWriteOneBitValueResponse),
+                std::make_tuple(PowerSupplyParameter::IS_ON, 0,
+                                slaveAddr
+                                + modbusWriteCoils
+                                + "0000" // Address of first coil to be written.
+                                + "0003" // Quantity of coils.
+                                + "01" // Byte count.
+                                + "00" //Coils' values.
+                                + "8F57" // CRC
+                        , correctWriteOneBitValueResponse),
                 std::make_tuple(PowerSupplyParameter::POLARITY, 1,
                                 slaveAddr
                                 + modbusWriteCoils
@@ -102,7 +162,16 @@ INSTANTIATE_TEST_SUITE_P(
                                 + "01" // Byte count.
                                 + "05" //Coils' values.
                                 + "4F54" // CRC
-                        , correctSwitchOnOffResponse),
+                        , correctWriteOneBitValueResponse),
+                std::make_tuple(PowerSupplyParameter::POLARITY, 0,
+                                slaveAddr
+                                + modbusWriteCoils
+                                + "0000" // Address of first coil to be written.
+                                + "0003" // Quantity of coils.
+                                + "01" // Byte count.
+                                + "01" //Coils' values.
+                                + "4E97" // CRC
+                        , correctWriteOneBitValueResponse),
                 std::make_tuple(PowerSupplyParameter::RESET, 1,
                                 slaveAddr
                                 + modbusWriteCoils
@@ -111,7 +180,16 @@ INSTANTIATE_TEST_SUITE_P(
                                 + "01" // Byte count.
                                 + "06" //Coils' values.
                                 + "0F55" // CRC
-                        , correctSwitchOnOffResponse)
+                        , correctWriteOneBitValueResponse),
+                std::make_tuple(PowerSupplyParameter::RESET, 0,
+                                slaveAddr
+                                + modbusWriteCoils
+                                + "0000" // Address of first coil to be written.
+                                + "0003" // Quantity of coils.
+                                + "01" // Byte count.
+                                + "02" //Coils' values.
+                                + "0E96" // CRC
+                        , correctWriteOneBitValueResponse)
         ));
 
 
