17296a494063f3b2a8cc7fed5cb227469a2a04d4
Agnieszka Klempis <a.klempis@student.uw.edu.pl>
2024-03-14 18:53:49 +0100

    Fix UART read and write

--- src/picoController/PicoController.cpp
+++ src/picoController/PicoController.cpp
@@ -1,26 +1,23 @@
 #include "PicoController.hpp"
 #include <unistd.h>
-#include <cstdio>
 
 static int32_t read_serial_stdin(uint8_t *buf, uint16_t count, int32_t byte_timeout_ms, void *arg) {
 
     for (int i = 0; i < count; i++) {
-        int c = getchar_timeout_us(PicoController::GET_ONE_BYTE_TIMEOUT); // TODO: Consider using stdio_set_chars_available_callback()
 
-        if (c == PICO_ERROR_TIMEOUT || c == PicoController::ENDSTDIN)
+        if (!uart_is_readable_within_us(UART_ID, PicoController::GET_ONE_BYTE_TIMEOUT))
             return i;
 
-        buf[i] = c;
+        uart_read_blocking(UART_ID, buf + i, 1);
     }
     return count;
 }
 
 static int32_t write_serial_stdout(const uint8_t *buf, uint16_t count, int32_t byte_timeout_ms, void *arg) {
 
-    fflush(stdout);
+    gpio_put(PicoController::LED_PIN, 1);
 
-    if (write(STDOUT_FILENO, buf, count) < 0)
-        return -1;
+    uart_write_blocking(UART_ID, buf, count);
 
     return count;
 }
--- src/picoController/PicoController.hpp
+++ src/picoController/PicoController.hpp
@@ -6,7 +6,17 @@
 extern "C" {
 #include "pico/stdlib.h"
 #include "pico/stdio.h"
+#include "hardware/uart.h"
+#include "hardware/irq.h"
 }
+
+#define UART_ID uart0
+#define BAUD_RATE 115200
+#define DATA_BITS 8
+#define STOP_BITS 1
+#define PARITY    UART_PARITY_NONE
+#define UART_TX_PIN 0
+#define UART_RX_PIN 1
 
 class PicoController : public PicoControllerInterface {
 
@@ -20,9 +30,31 @@ public:
     static const uint8_t ENDSTDIN = 255;
 
     PicoController() {
+        // Set up our UART with a basic baud rate.
+        uart_init(UART_ID, 2400);
+
+        // Set the TX and RX pins by using the function select on the GPIO
+        // Set datasheet for more information on function select
+        gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
+        gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
+
+        // Actually, we want a different speed
+        // The call will return the actual baud rate selected, which will be as close as
+        // possible to that requested
+        int __unused actual = uart_set_baudrate(UART_ID, BAUD_RATE);
+
+        // Set UART flow control CTS/RTS, we don't want these, so turn them off
+        uart_set_hw_flow(UART_ID, false, false);
+
+        // Set our data format
+        uart_set_format(UART_ID, DATA_BITS, STOP_BITS, PARITY);
+
+        // Turn off FIFO's - we want to do this character by character
+        uart_set_fifo_enabled(UART_ID, false);
+
         gpio_init(LED_PIN);
         gpio_set_dir(LED_PIN, GPIO_OUT);
-        gpio_put(LED_PIN, 1);
+        gpio_put(LED_PIN, 0);
     }
 
     void onError() override {
