66ccd002bc17c54418f879bcbca6bb5a6cd8a781
Agnieszka Klempis <a.klempis@student.uw.edu.pl>
2024-03-15 09:51:51 +0100

    Move uart parameters to CMakeLists

--- src/picoController/PicoController.hpp
+++ src/picoController/PicoController.hpp
@@ -11,23 +11,14 @@ extern "C" {
 }
 
 #define UART_ID uart0
-#define BAUD_RATE 115200
 #define DATA_BITS 8
 #define STOP_BITS 1
 #define PARITY    UART_PARITY_NONE
-#define UART_TX_PIN 0
-#define UART_RX_PIN 1
 
 class PicoController : public PicoControllerInterface {
 
-private:
-
-    uint gpio_in_pin_;
-
 public:
     static const int LED_PIN = PICO_DEFAULT_LED_PIN;
-
-    static const uint8_t ENDSTDIN = 255;
 
     PicoController() {
         // Set up our UART with a basic baud rate.
