1be19e774a881ae781d5a5ec338621a9710f933c
Agnieszka Klempis <a.klempis@student.uw.edu.pl>
2024-03-25 21:34:25 +0100

    Add communication with ps interval and switch with ps model in main.

--- src/picoController/PicoController.cpp
+++ src/picoController/PicoController.cpp
@@ -25,8 +25,11 @@ static int32_t readUart(uint8_t *buf, uint16_t count, int32_t byte_timeout_ms, b
 
     for (int i = 0; i < count; i++) {
 
-        if (!first_byte_from_msg && !uart_is_readable_within_us(UART_ID, PicoController::GET_ONE_BYTE_TIMEOUT))
+        int timeout = first_byte_from_msg ? COMMUNICATION_WITH_PS_INTERVAL : PicoController::GET_ONE_BYTE_TIMEOUT;
+
+        if (!first_byte_from_msg && !uart_is_readable_within_us(UART_ID, timeout)) {
             return i;
+        }
 
         uart_read_blocking(UART_ID, buf + i, 1);
     }
